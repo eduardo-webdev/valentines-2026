f6a55a93dcb01c7be4fe15ec9c7a3447c0ebd0c0
avalos-eduardo <178445917+avalos-eduardo@users.noreply.github.com>
2026-02-11 00:12:57 -0600

    gif extension correction

--- src/routes/Success.tsx
+++ src/routes/Success.tsx
@@ -9,7 +9,7 @@ export default function Success() {
         particleCount={100}
         colors={["#ff6b6b", "#4ecdc4", "#ffe66d"]}
       />
-      <img src="src/assets/celebrate.gif" alt="Success" className="w-70 h-70" />
+      <img src="src/assets/celebrate.GIF" alt="Success" className="w-70 h-70" />
       <h1 className="text-center text-4xl font-bold text-[#e7e1e1] pt-8 px-5 absolute top-10">
         THANK YOUUUU!! I HOPE YOU ENJOY THE DONUTS ❤️❤️❤️
       </h1>
